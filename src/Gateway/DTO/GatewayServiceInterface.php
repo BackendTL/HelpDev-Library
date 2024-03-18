07c69d932c44a9890e7babddee1b3413efd0445f
Alex TL <backend.tl.it@gmail.com>
2024-03-18 18:15:52 +0200

    added new contants for interface

--- src/Gateway/DTO/GatewayServiceInterface.php
+++ src/Gateway/DTO/GatewayServiceInterface.php
@@ -6,6 +6,13 @@ use DateTimeImmutable;
 
 interface GatewayServiceInterface
 {
+    public const SERVICE_FRONTEND = 'frontend';
+    public const SERVICE_USER = 'user_service_api';
+    public const SERVICE_EXIF_CLEANER = 'exif_cleaner_service_api';
+    public const SERVICE_TRADEMARK = 'trademarks_service_api';
+    public const SERVICE_CONTENT_PARSER = 'content_parser_service_api';
+    public const SERVICE_NODEJS_PARSER = 'nodejs_parser_api';
+
     public function getId(): int;
 
     public function getName(): string;
