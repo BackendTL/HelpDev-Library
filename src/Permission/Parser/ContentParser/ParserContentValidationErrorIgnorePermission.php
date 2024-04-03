49f373438cd6f7318708c18471a06d9338b71a41
Alex TL <backend.tl.it@gmail.com>
2024-04-03 17:59:32 +0300

    Added permissions for content parsing (validation errors add ignore and remove errors)

---
+++ src/Permission/Parser/ContentParser/ParserContentValidationErrorIgnorePermission.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace HelpDev\Permission\Parser\ContentParser;
+
+use HelpDev\Permission\PermissionInterface;
+
+class ParserContentValidationErrorIgnorePermission implements PermissionInterface
+{
+    public const KEY = '3.1.5';
+    public const DESCRIPTION = 'Позволяет игнорить ошибки валидации';
+
+    public static function getKey(): string
+    {
+        return self::KEY;
+    }
+
+    public static function getDescription(): string
+    {
+        return self::DESCRIPTION;
+    }
+}
